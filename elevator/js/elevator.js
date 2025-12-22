2094f75f8ae61cc92b5e98edb354292d0a49cdfc
a <github1@alexyip.com>
2025-12-22 16:11:40 -0500

    a

--- elevator/js/elevator.js
+++ elevator/js/elevator.js
@@ -1,4 +1,4 @@
-import Queue from './queue.js';
+import ElevatorQueue, { FloorQueue } from './queue.js';
 
 class Person {
     constructor(name, originFloor, destinationFloor) {
@@ -11,35 +11,125 @@ class Person {
     }
 }
 
+const DIRECTION = Object.freeze({
+    UP: 100,
+    DOWN: 200
+});
+
 class Elevator {
-    constructor(capacity, loadingTime) {
+    constructor(capacity, floors) {
         this.capacity = capacity; // in number of people
-        this.loadingTime = loadingTime; // in seconds
-        this.people = []; // current list of people
+        this.floors = floors; // list of floors
+        this.queue = new ElevatorQueue(); // current list of people
         this.currentFloor = 0; // current floor
         this.destinationFloor = 0; // destination floor
     }
+
+    isFull() {
+        return this.queue.size() >= this.capacity;
+    }
+
+    /**
+     * @returns false if elevator didn't move (is at destination), 
+     * otherwise it moves the elevator up or down one floor and returns true
+     */
+    move() {
+        if (this.currentFloor === this.destinationFloor) {
+            return false;
+        } else {
+            if (this.currentFloor < this.destinationFloor) {
+                this.currentFloor++;
+            } else {
+                this.currentFloor--;
+            }
+        }
+        return true;
+    }
+
+    getNextDestinationFloor() {
+        return this.queue.peek().destinationFloor; // get the destination floor of the next person in the queue
+    }
+
+    getNextDirection() {
+        nextDestinationFloor = this.getNextDestinationFloor();
+        if (nextDestinationFloor > this.currentFloor) {
+            return DIRECTION.UP;
+        } else {
+            return DIRECTION.DOWN;
+        }
+    }
+
+    /**
+     * @description Unloads people from the elevator if they have reached their destination floor one at
+     * a time.
+     * @returns true if someone was unloaded, otherwise false
+     */
+    unLoadPeople() {
+        const person = this.queue.dequeue(this.currentFloor);
+        if (person != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @returns true if someone was loaded, otherwise false
+     */
+    loadPeople() {
+        if (!this.isFull()) {
+            let person = null;
+            if (this.getNextDirection() === DIRECTION.UP) {
+                person = this.floors[this.currentFloor].queue.dequeueNextWithDestinationAbove(this.currentFloor);
+            } else {
+                //going down
+                person = this.floors[this.currentFloor].queue.dequeueNextWithDestinationBelow(this.currentFloor);
+            }
+            if (person == null) {
+                //no one on this floor going in the same direction
+                return false;
+            } else {
+                //person enters the elevator
+                this.queue.enqueue(person);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    moveAndLoad() {
+        if (!this.move()) {
+            //elevator didn't move, so it's at its destination floor. need to unload and load people
+            if (!this.unLoadPeople()) {
+                //no one to unload, so need to load people
+                if (!this.loadPeople()) {
+                    //no one to load
+                    return;
+                }
+            }
+        }
+    }
+
 }
 
 class RegularElevator extends Elevator {
-    constructor() {
-        super(4, 3); //4 people and 3 seconds to load
+    constructor(floors) {
+        super(4, floors); //4 person capacity
     }
 }
 
 class Floor {
     constructor(floorNumber) {
         this.floorNumber = floorNumber; // floor number
-        this.people = [];
+        this.queue = new FloorQueue();
     }
     getPeopleOnFloor() {
-        return this.people.length;
+        return this.queue.size();
     }
     addPerson(person) {
-        this.people.push(person);
+        this.queue.enqueue(person);
     }
     removePerson(person) {
-        this.people = this.people.filter(p => p !== person);
     }
 }
 
@@ -53,21 +143,31 @@ class Building {
             this.floors.push(new Floor(i));
         }
         for (let i = 0; i < numElevators; i++) {
-            this.elevators.push(new RegularElevator());
+            this.elevators.push(new RegularElevator(this.floors));
         }
     }
+
+    moveElevatorsAndLoad() {
+        for (let elevator of this.elevators) {
+            //move each elevator
+            elevator.moveAndLoad();
+        }
+    }
+
     getFloor(floorNumber) {
         return this.floors.find(floor => floor.floorNumber === floorNumber);
     }
+
     getNumberFloors() {
         return this.floors.length;
     }
+
     /**
      * @description Generates people and places them on random floors in the building.
      * @param {number} numPeople - The number of people to generate and place in the building.
      */
     generatePeopleRandomly(numPeople) {
-        const names = ["a", "b", "c", "d", "e", "f", "g", "h"];
+        const names = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
         for (let i = 0; i < numPeople; i++) {
             const name = names[Math.floor(Math.random() * names.length)];
             const originFloor = Math.floor(Math.random() * this.getNumberFloors());
@@ -94,9 +194,9 @@ class Simulation {
         // Implement the logic to simulate one step of the simulation
         // generate people randomly
         this.building.generatePeopleRandomly(this.peoplePerSecond);
-        // update the state of the building and elevators
-        // check if all people have reached their destination
-        console.log(this.building);
+        // move elevators to their destination floors
+        // load and unload people from the elevators
+        this.building.moveElevatorsAndLoad();
     }
     run() {
         setInterval(() => this.step(), 1000);
--- elevator/js/queue.js
+++ elevator/js/queue.js
@@ -1,37 +1,77 @@
-
-export default class Queue {
+export class Queue {
     constructor() {
-        this.storage = [];
+        this.people = [];
     }
 
     // Add an element to the end of the queue
     enqueue(element) {
-        this.storage.push(element);
+        this.people.push(element);
     }
 
     // Remove and return the first element from the queue
     dequeue() {
         if (this.isEmpty()) {
-            return "Queue is empty";
+            return null;
         }
-        return this.storage.shift();
+        return this.people.shift();
     }
 
     // Return the first element without removing it
     peek() {
         if (this.isEmpty()) {
-            return "Queue is empty";
+            return null;
         }
-        return this.storage[0];
+        return this.people[0];
     }
 
     // Check if the queue is empty
     isEmpty() {
-        return this.storage.length === 0;
+        return this.people.length === 0;
     }
 
     // Return the size of the queue
     size() {
-        return this.storage.length;
+        return this.people.length;
+    }
+}
+
+export class FloorQueue extends Queue {
+    dequeueNextWithDestinationAbove(floor) {
+        if (!this.isEmpty()) {
+            //find first person in queue who's destination is above the current floor
+            for (let i = 0; i < this.people.length; i++) {
+                if (this.people[i].destinationFloor > floor) {
+                    return this.people.splice(i, 1)[0];
+                }
+            }
+        }
+        return null;
+    }
+
+    dequeueNextWithDestinationBelow(floor) {
+        if (!this.isEmpty()) {
+            //find first person in queue who's destination is below the current floor
+            for (let i = 0; i < this.people.length; i++) {
+                if (this.people[i].destinationFloor < floor) {
+                    return this.people.splice(i, 1)[0];
+                }
+            }
+        }
+        return null;
+    }
+}
+
+export class ElevatorQueue extends Queue {
+    // Remove and return the first element from the queue
+    dequeue(floor) {
+        if (!this.isEmpty()) {
+            //find first person in queue who's destination matches the current floor
+            for (let i = 0; i < this.people.length; i++) {
+                if (this.people[i].destinationFloor === floor) {
+                    return this.people.splice(i, 1)[0];
+                }
+            }
+        }
+        return null;
     }
 }
